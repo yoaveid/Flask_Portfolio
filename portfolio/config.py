b61a76bf29c8ac7a069759396bf1f7093aa138d6
yoaveid <100868331+yoaveid@users.noreply.github.com>
2023-12-03 14:22:18 +0200

    Update config.py

--- portfolio/config.py
+++ portfolio/config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY_portfolio')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
