bead327508cee2193ec9695d1360ef8b52783901
yoaveid <100868331+yoaveid@users.noreply.github.com>
2024-11-15 11:00:12 +0200

    Update config.py

--- portfolio/config.py
+++ portfolio/config.py
@@ -2,9 +2,9 @@ import os
 class Config:
     #SECRET_KEY = os.environ.get('SECRET_KEY_portfolio')
     SECRET_KEY = 'dummy secret key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
+    SQLALCHEMY_DATABASE_URI = 'fake:///site.db'
+    MAIL_SERVER = 'fake.googlemail.com'
+    MAIL_PORT = 000
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = "MAIL_USER_NAME"
